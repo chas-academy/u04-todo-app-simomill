d784d13d499ee599a9189312cc7f17d150968e48
Simon Miller <mail@simonmiller.se>
2022-01-13 21:24:03 +0100

    Add urgency-sections

--- app/public/index.php
+++ app/public/index.php
@@ -26,29 +26,71 @@ $tasks = $statement->fetchAll(PDO::FETCH_ASSOC);
 
     <a href="add.php" class="add btn">Add Task</a>
 
-    
-    <div class="item heading">
+    <?php 
+
+        $today = 0;
+        
+        foreach ($tasks as $i => $task) {
+            if ($task['urgency'] === "Today") {
+                $today ++ ;   
+            }};
+
+    if ($today !== 0): ?>
+        
+        <h2>Today</h2>
+
+        <div class="item heading">
             <span>Task</span>
             <span>Description</span>
             <span>Urgency</span>
             <span>Created</span>
             <span>Delete</span>
             <span>Edit</span>
-            <span>Done</span>
-            
+            <span>Done</span>  
         </div>
+        
+    <?php else: ?>
+        <h2>Nothing needs to be done today.</h2>
+    <?php endif; ?>
 
-        <?php foreach ($tasks as $i => $task): ?>
-            <?php if ($task['urgency'] === null): ?>
-            <?php require '../partials/_tableLoop.php'; ?>
-            <?php endif; ?>
-        <?php endforeach; ?>
+    <?php foreach ($tasks as $i => $task): ?>
+        <?php if ($task['urgency'] === null): ?>
+        <?php require '../partials/_tableLoop.php'; ?>
+        <?php endif; ?>
+    <?php endforeach; ?>
 
-        <?php foreach ($tasks as $i => $task): ?>
-            <?php if ($task['urgency'] === 'Today'): ?>
-            <?php require '../partials/_tableLoop.php'; ?>
-            <?php endif; ?>
-        <?php endforeach; ?>
+    <?php foreach ($tasks as $i => $task): ?>
+        <?php if ($task['urgency'] === 'Today'): ?>
+        <?php require '../partials/_tableLoop.php'; ?>
+        <?php endif; ?>
+    <?php endforeach; ?>
+
+    <?php 
+
+    $thisW = 0;
+    
+    foreach ($tasks as $i => $task) {
+        
+        if ($task['urgency'] === "This Week") {
+            $thisW ++ ;   
+        }
+    };
+
+    if ($thisW !== 0): ?>
+        
+        <h2>This Week</h2>
+
+        <div class="item heading">
+            <span>Task</span>
+            <span>Description</span>
+            <span>Urgency</span>
+            <span>Created</span>
+            <span>Delete</span>
+            <span>Edit</span>
+            <span>Done</span>  
+        </div>
+        
+    <?php endif; ?>
         
         <?php foreach ($tasks as $i => $task): ?>
             <?php if ($task['urgency'] === 'This Week'): ?>
